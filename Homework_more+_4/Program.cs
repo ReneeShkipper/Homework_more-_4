f14675a8848c024446fe5c56fb922b88edf79a10
ReneeShkipper <marinkagnome@gmail.com>
2023-12-10 18:55:10 +0300

    Add project files.

---
+++ Homework_more+_4/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Input sequence length");
+        int length = int.Parse(Console.ReadLine()); 
+        int result = CountVariations(length);
+
+        Console.WriteLine($"Number of variations: {result}");
+    }
+
+    static int CountVariations(int length)
+    {
+        if (length < 1)
+        {
+            return 0;
+        }
+
+        int[] variations = new int[length];
+
+        variations[0] = 2;
+        variations[1] = 3;
+
+        for (int i = 2; i < length; i++)
+        {
+            variations[i] = variations[i - 1] + variations[i - 2];
+        }
+
+        return variations[length - 1];
+    }
+}
